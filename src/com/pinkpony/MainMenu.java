b43ff830590ca961cf0aee850f3dd186ad8ca0b8
Gabe <gabe1118@yahoo.com>
2014-02-15 07:49:01 -0500

    We will deal with Facebook later

--- src/com/pinkpony/MainMenu.java
+++ src/com/pinkpony/MainMenu.java
@@ -3,12 +3,21 @@ package com.pinkpony;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import facebook4j.Account;
 import facebook4j.Facebook;
+import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
+import facebook4j.ResponseList;
+import facebook4j.auth.AccessToken;
 
 public class MainMenu extends AbstractGameState{
 
 	Graphics gr;
+	
+	String appId= "596061157140744";
+	String appSecret="5489721f19d79061fc9b29f0724f158f";
+	String accessToken="CAAIeHU2JKQgBAF2bRCqOZC9200IPUeiBFcM0yCvq4hXepBkvqFoSZCAflJEvud20ubYDhnBh8WyV91A8FwynqT8RZAgVus7n9cLzDi79xZCwhfD4TVgHjcW8deEuBvjvwxrDMV03ZCLHZCAS9XSIzqSvZBgxH8k2ipa1kFquLDXbKG6htj8G97uOlGt2nZBiFxZCgUGxGODOEZAgZDZD";
+	String permissions= "email";
 	
 	public void tick(Graphics g)
 	{
@@ -29,12 +38,19 @@ public class MainMenu extends AbstractGameState{
 		StateChanger.gameState = GameState.PLAY;
 	}
 	
-	public void facebookLogin()
+	/*
+	public void facebookLogin() throws FacebookException
 	{
 		Facebook facebook = new FacebookFactory().getInstance();
+		facebook.setOAuthAppId(appId, appSecret);
+		facebook.setOAuthPermissions(permissions);
+		facebook.setOAuthAccessToken(new AccessToken(accessToken, null));
 		
-		
+		ResponseList<Account> accounts = facebook.getAccounts();
+		Account yourPageAccount = accounts.get(0);  // if index 0 is your page account.
+		String pageAccessToken = yourPageAccount.getAccessToken();
+		facebook.postStatusMessage("Hello World from Facebook4J.");
 	}
-	
+	*/
 	
 }
